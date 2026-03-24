e569a671c219e81b88eeb41945b9b435050a1ab5
ssshaise <ruchir412393@gmail.com>
2026-03-24 18:36:11 +0530

    Fixed HuggingSpaces Error

--- src/frontend/src/api.ts
+++ src/frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_URL = (import.meta.env.VITE_API_URL || "https://ssshaise-rcie.hf.space").replace(/\/$/, "");
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://ssshaise-rcie.hf.space";
 
 type Edge = string[];
 
